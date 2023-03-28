8d8f97c3864d680074cbdc12206e896e22cfd04a
Mitsuharu Seki <mitsu1986@gmail.com>
2023-03-28 09:06:08 +0000

    Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM alpine:3.17
+
+RUN apk add --update --no-cache krb5 ldb-tools samba-dc samba-winbind-clients tdb \
+      bind bind-libs bind-tools libcrypto1.1 libxml2 tzdata py3-setuptools py3-pip && \
+    pip install j2cli && \
+    apk del py3-pip
+
+RUN rm -rf /etc/samba && \
+    rm -rf /var/lib/samba && \
+    ln -s /samba/etc /etc/samba && \
+    ln -s /samba/lib /var/lib/samba && \
+    mkdir -p /samba/etc && \
+    mkdir -p /samba/lib
+
+ADD entrypoint.sh /usr/local/bin/
+RUN chmod a+x /usr/local/bin/entrypoint.sh
+
+VOLUME /samba
+EXPOSE 53 53/udp 88 88/udp 135 137-138/udp 139 389 389/udp 445 464 464/udp 636 3268-3269 49152-49200
+
+ENV REALM=ad.example.com
+ENV ADMIN_PASSWORD=admin1234!
+ENV DOMAIN=EXAMPLE
+ENV DNS_BACKEND=SAMBA_INTERNAL
+ENV RPC_PORTS=49152-49200
+
+ENTRYPOINT [ "/usr/local/bin/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$NETBIOS_NAME" ]; then
+  NETBIOS_NAME=$(hostname -s | tr [a-z] [A-Z])
+else
+  NETBIOS_NAME=$(echo $NETBIOS_NAME | tr [a-z] [A-Z])
+fi
+
+REALM=$(echo "$REALM" | tr [a-z] [A-Z])
+
+if [ ! -f /etc/timezone ] && [ ! -z "$TZ" ]; then
+  echo 'Set timezone'
+  cp /usr/share/zoneinfo/$TZ /etc/localtime
+  echo $TZ >/etc/timezone
+fi
+
+if [ -n "$HOST_IPV4ADDR" ] && [ -n "$HOST_IPV6ADDR" ]; then
+  HOSTIP_OPTION="--host-ip=\"${HOST_IPV4ADDR}\" --host-ip6=\"${HOST_IPV6ADDR}\" --option=\"dns update command = /usr/sbin/samba_dnsupdate --current-ip ${HOST_IPV4ADDR} --current-ip ${HOST_IPV6ADDR}\""
+elif [ -n "$HOST_IPV4ADDR" ]; then
+  HOSTIP_OPTION="--host-ip=\"${HOST_IPV4ADDR}\" --option=\"dns update command = /usr/sbin/samba_dnsupdate --current-ip ${HOST_IPV4ADDR}\""
+elif [ -n "$HOST_IPV6ADDR" ]; then
+  HOSTIP_OPTION="--host-ip6=\"${HOST_IPV6ADDR}\" --option=\"dns update command = /usr/sbin/samba_dnsupdate --current-ip ${HOST_IPV6ADDR}\""
+else
+  HOSTIP_OPTION=""
+fi
+
+if [ -n "$DNS_FORWARDER" ]; then
+  DNS_FORWARDER_OPTION="--option=\"dns forwarder = ${DNS_FORWARDER}\""
+else
+  DNS_FORWARDER_OPTION=""
+fi
+
+# Configure the AD DC
+if [ ! -f /var/lib/samba/registry.tdb ]; then
+    echo "${DOMAIN} - Begin Domain Provisioning"
+    rm -f /etc/samba/smb.conf /etc/krb5.conf
+    samba-tool domain provision \
+        --domain="${DOMAIN}" \
+        --adminpass="${ADMIN_PASSWORD}" \
+        --server-role=dc \
+        --realm="${REALM}" \
+        --dns-backend="${DNS_BACKEND}" \
+        --use-rfc2307 \
+        ${DNS_FORWARDER_OPTION} \
+        ${HOSTIP_OPTION} \
+        --option="rpc server dynamic port range = ${RPC_PORTS}" \
+        --host-name="${NETBIOS_NAME}" \
+        ${ADDITIONAL_OPTIONS}
+    echo "${DOMAIN} - Domain Provisioned Successfully"
+fi
+
+samba -i
